32abd42bc1a888bcb67b21e34709f1df4cf6bb66
Kristiyan Stoimenov <kristoimenov@gmail.com>
2023-12-10 18:21:50 +0200

    day10: Add solution to simple-input2 from part1
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- day10/pipe.cpp
+++ day10/pipe.cpp
@@ -1,16 +1,18 @@
-#include <cassert>
-#include <cstdint>
-#include <algorithm>
-#include <vector>
-#include <optional>
-#include <queue>
-#include <unordered_map>
-#include <string>
-#include <fstream>
-#include <iostream>
+#include <algorithm>  // for max, copy, fill_n
+#include <cassert>    // for assert
+#include <fstream> 	  // for ifstream
+#include <cstdint>    // for uint64_t, int64_t
+#include <cstdlib>    // for labs
+#include <iostream>   // for operator<<, cout, basic_istream, istream, ifstream
+#include <optional>   // for optional, make_optional
+#include <queue>      // for queue
+#include <string>     // for basic_string, getline, string
+#include <utility>    // for swap
+#include <vector>     // for vector
 
 template <typename T>
-T takeone(std::queue<T> &q) {
+T takeone(std::queue<T>& q)
+{
 	auto to_pop = q.front();
 	q.pop();
 	return to_pop;
@@ -22,58 +24,84 @@ struct maze {
 
 	struct coordinate {
 		char curr;
-		std::uint64_t row;
-		std::uint64_t col;
+		std::int64_t row;
+		std::int64_t col;
 		std::uint64_t depth;
 
-		std::vector<coordinate> children(const maze &mz) {
+		std::vector<coordinate> children(const maze& mz)
+		{
 			std::vector<coordinate> children_;
 			auto sym_at = [&](std::uint64_t r, std::uint64_t c) {
 				return mz.maze[r].at(c);
 			};
 
+			auto find_vertical = [&](int step) -> std::optional<coordinate> {
+				std::int64_t r = row + step;
+				for (; r > 0 && r <= ROWS && sym_at(r, col) == '|'; r += step)
+					;
+				if (r < 0 || r >= ROWS || sym_at(r, col) == '.')
+					return {};
+				return coordinate { sym_at(r, col), r, col, depth + std::labs((r - row)) };
+			};
+
+			auto find_horizontal = [&](int step) -> std::optional<coordinate> {
+				std::int64_t c = col + step;
+				for (; c > 0 && c <= COLS && sym_at(row, c) == '-'; c += step)
+					;
+				if (c < 0 || c >= COLS || sym_at(row, c) == '.')
+					return {};
+				return std::make_optional(coordinate { sym_at(row, c), row, c, depth + std::labs((c - col)) });
+			};
+
 			switch (curr) {
-				break; case 'L': // North-East
-					children_.emplace_back(sym_at(row - 2, col), row - 2, col, depth + 2); // North
-					children_.emplace_back(sym_at(row, col + 2), row, col + 2, depth + 2); // East
-				break; case 'J': // North-West
-					children_.emplace_back(sym_at(row - 2, col), row - 2, col, depth + 2); // North
-					children_.emplace_back(sym_at(row, col - 2), row, col - 2, depth + 2); // West
-				break; case '7': // South-West
-					children_.emplace_back(sym_at(row + 2, col), row + 2, col, depth + 2); // South
-					children_.emplace_back(sym_at(row, col - 2), row, col - 2, depth + 2); // West
-				break; case 'F': // South-East
-					children_.emplace_back(sym_at(row + 2, col), row + 2, col, depth + 2); // South
-					children_.emplace_back(sym_at(row, col + 2), row, col + 2, depth + 2); // East
-				break; case 'S':
-					if (sym_at(row + 1, col) == '|')
-						children_.emplace_back(sym_at(row + 2, col), row + 2, col, depth + 2);
-					if (sym_at(row - 1, col) == '|')
-						children_.emplace_back(sym_at(row - 2, col), row - 2, col, depth + 2);
-					if (sym_at(row, col + 1) == '-')
-						children_.emplace_back(sym_at(row, col + 2), row, col + 2, depth + 2);
-					if (sym_at(row, col - 1) == '-')
-						children_.emplace_back(sym_at(row, col - 2), row, col - 2, depth + 2);
-				break; case '.': // nothing
-				default:
-					__builtin_unreachable();
+			case 'L':										  // North-East
+				children_.emplace_back(*find_vertical(-1));	  // North
+				children_.emplace_back(*find_horizontal(+1)); // East
+				break;
+			case 'J':										  // North-West
+				children_.emplace_back(*find_vertical(-1));	  // North
+				children_.emplace_back(*find_horizontal(-1)); // West
+				break;
+			case '7':										  // South-West
+				children_.emplace_back(*find_vertical(+1));	  // South
+				children_.emplace_back(*find_horizontal(-1)); // West
+				break;
+			case 'F':										  // South-East
+				children_.emplace_back(*find_vertical(+1));	  // South
+				children_.emplace_back(*find_horizontal(+1)); // East
+				break;
+			case 'S':
+				if (auto v = find_vertical(+1); v)
+					children_.push_back(*v);
+				if (auto v = find_vertical(-1); v)
+					children_.push_back(*v);
+				if (auto h = find_horizontal(+1); h)
+					children_.push_back(*h);
+				if (auto h = find_horizontal(-1); h)
+					children_.push_back(*h);
+				break;
+			case '.': // nothing
+			default:
+				__builtin_unreachable();
 			}
 
 			assert(children_.size() == 2);
+			if (children_[0].depth > children_[1].depth)
+				std::swap(children_[0], children_[1]);
 			return children_;
 		}
-
 	};
 
-	friend std::istream &operator>>(std::istream &is, maze &m) {
-		std::uint64_t i = 0;
+	friend std::istream& operator>>(std::istream& is, maze& m)
+	{
+		std::int64_t i = 0;
 		std::optional<coordinate> maybe_start;
 		for (std::string line; std::getline(is, line); ++i) {
 			assert(line.size() == COLS);
 			m.maze.push_back(line);
 			if (!maybe_start)
-				if (const auto p = line.find('S'); p != std::string::npos)
-					maybe_start.emplace('S', i, p, 0);
+				if (const std::int64_t p = line.find('S'); p != std::string::npos)
+					maybe_start.emplace(coordinate{'S', i, p, 0});
 		}
 
 		assert(m.maze.size() == ROWS);
@@ -82,25 +110,35 @@ struct maze {
 		return is;
 	}
 
-	std::uint64_t longest_path() {
-		std::vector<bool> visited(ROWS * COLS, false);
-		auto mark_visited = [&](const coordinate &c) {
-			visited[c.col * ROWS + c.row] = true;
+	std::uint64_t longest_path()
+	{
+		std::vector<char> visited(ROWS * COLS, '.');
+		auto mark_visited = [&](const coordinate& c) {
+			visited[c.row * COLS + c.col] = c.curr;
 		};
-		auto is_visited = [&](const coordinate &c) {
-			return visited[c.col * ROWS + c.row];
+		auto is_visited = [&](const coordinate& c) {
+			return visited[c.row * COLS + c.col] != '.';
+		};
+		auto show_visited = [&]() {
+			for (std::uint64_t j = 0; j < ROWS; ++j) {
+				for (std::uint64_t i = 0; i < COLS; ++i)
+					std::cout << visited[j * COLS + i];
+				std::cout << '\n';
+			}
+			std::cout << '\n';
 		};
 
 		std::queue<coordinate> wset;
 		wset.push(start);
 		mark_visited(start);
 
-		std::uint64_t max_depth{0};
+		std::uint64_t max_depth { 0 };
 		while (!wset.empty()) {
 			auto curr = takeone(wset);
+			show_visited();
 			max_depth = std::max(curr.depth, max_depth);
-			const auto &children = curr.children(*this);
-			for (const auto &c : children) {
+			const auto& children = curr.children(*this);
+			for (const auto& c : children) {
 				if (is_visited(c))
 					continue;
 				wset.push(c);
